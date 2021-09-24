427a3662f73ced17c79182be89658c984f5a67db
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2021-09-24 19:52:24 +0900

    dddddd

--- 서울버스 조회.py
+++ 서울버스 조회.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup   #라이브러리 import
 from urllib.request import urlopen  #url라이브러리 불러옴
 
 
-
-
 busId = input("버스 노선 Id를 입력하시오 : ")
 open_api_key = '46rvMskXxMWkqJuYkMxwx%2FZnnKSI0pO%2FOGiu3%2FCVSsUlEdoCAMWXfC%2Bk5DRGlrorCjbvJbb2ZcbPbWh8ZxNY6Q%3D%3D'
 params = '&busRouteId=' + busId
@@ -11,7 +9,7 @@ open_url = urlopen('http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceK
 
 num = 1
 
-#test
+#11
 
 soup = BeautifulSoup(open_url, 'lxml')
 for anchor in soup.select('gpsY, gpsX, plainNo, dataTm'):
